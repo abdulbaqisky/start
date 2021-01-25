c60a09170f687be5b20473b7ba448d71a7279ad3
Adekomi AbdulBaaqi <adekomiabdulbaqi3@gmail.com>
2021-01-25 13:26:05 +0100

    testing typecasting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int valA = 21;
-        int valB = 6;
-        int valC = 3;
-        int valD = 1;
+        float floatVal = 1.0f;
+        double doubleVal = 4.0d;
+        byte byteVal = 7;
+        short shortVal = 7;
+        long longVal = 5;
 
-        int result1 = valA + valB;
+        short result1 = byteVal ;
+        short result2 = (short) longVal;
+        short result3 = (short) doubleVal;
+        short result4 = (short) floatVal;
 
-        int result2 = (valA - valB) / valC + valD;
+        long result5 = shortVal;
+        long result6 = (long) doubleVal;
+        long result7 = (long) floatVal;
+
+        float result8 = longVal;
+        float result9 = (float) doubleVal;
+        float result10 = (short) floatVal;
 
         System.out.println(result1);
         System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(result6);
+        System.out.println(result7);
+        System.out.println(result8);
+        System.out.println(result9);
+        System.out.println(result10);
+
     }
 }
