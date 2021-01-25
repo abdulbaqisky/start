95071593a29b53ee1901df6e59216a9e764606ef
Adekomi AbdulBaaqi <adekomiabdulbaqi3@gmail.com>
2021-01-25 12:58:38 +0100

    testing operator precedence

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int valA = 21;
+        int valB = 6;
+        int valC = 3;
+        int valD = 1;
+
+        int result1 = valA + valB;
+
+        int result2 = (valA - valB) / valC + valD;
+
+        System.out.println(result1);
+        System.out.println(result2);
     }
 }
